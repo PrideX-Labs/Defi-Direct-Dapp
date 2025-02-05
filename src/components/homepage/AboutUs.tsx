431b5039d7d7a733c53104e7f4c0bdf8091391f5
Rampop01 <popoolarahmat96@gmail.com>
2025-02-05 08:43:51 +0100

    Landing page/home page done

---
+++ src/components/homepage/AboutUs.tsx
@@ -0,0 +1,13 @@
+import { div } from 'framer-motion/client'
+import React from 'react'
+
+function AboutUs() {
+  return (
+    <div className='mx-64 pb-16'>
+        <h1 className="text-5xl ">AboutUs</h1>
+        <p>Spend directly from your DeFi wallet anywhere, anytime—no intermediaries, no delays. Secure, fast, and built for the future.</p>
+    </div>
+  )
+}
+
+export default AboutUs
