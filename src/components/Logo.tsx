9e8b76e93eebc99fe1edaa3096f947b7b6971046
ogazboiz <ogazboizakpolo@gmail.com>
2025-02-20 11:39:29 +0100

    dashboard in progress

---
+++ src/components/Logo.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Logo() {
+  return (
+    <div className='font-bold '>
+      <h1 className='text-3xl'>Defi-<span className='text-primary'>Direct</span></h1> 
+    </div>
+  )
+}
+
+export default Logo;
